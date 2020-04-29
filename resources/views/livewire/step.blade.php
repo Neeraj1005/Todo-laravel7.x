53ed0586aa2207c60b58dc591eab85036c80d470
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-04-29 09:12:39 +0530

    livewire issue fix

--- resources/views/livewire/step.blade.php
+++ resources/views/livewire/step.blade.php
@@ -4,9 +4,9 @@
             <span wire:click="increment" class="fas fa-plus-circle cursor-pointer py-3" />
     </div>
     @foreach($steps as $step)
-    <div>
-        <input type="text" name="step[]" class="border shadow appearance-none border rounded py-2 px-3" placeholder="{{'Steps'.$step}}" />
-        <span wire:click="decrement({{$loop->index}})" class="fas fa-times-circle text-red-400 cursor-pointer py-3" />
+    <div wire:key="{{$step}}">
+        <input type="text" name="step[]" class="border shadow appearance-none border rounded py-2 px-3" placeholder="{{'Steps'.($step + 1)}}" />
+        <span wire:click="decrement({{$step}})" class="fas fa-times-circle text-red-400 cursor-pointer py-3" />
     </div>
     @endforeach
 </div>
